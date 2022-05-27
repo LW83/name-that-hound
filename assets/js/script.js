bdb148008fdba73b9d5b22f3f8185d48fe60dfb9
LW83 <ljbwilkinson@gmail.com>
2022-05-27 14:31:15 +0000

    Add data-type and classes to HTML buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,32 @@
+let buttons = document.getElementsByTagName("button");
+let photo = document.getElementById("quiz-photo");
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let game = this.getAttribute("data-type");
+                alert(`You clicked ${generatePhoto}`);
+            }
+        });
+    }
+
+});
+
 let dogImages = [
     { name: "Afghan Hound", image: "afghan-hound.jpeg" },
     { name: "Airedale Terrier", image: "airedale-terrier.jpeg" } 
 ];
 
-function runGame() {
-
-}
-
-function generatephoto() {
-
+function generatePhoto() {
+    for (var i = 0; i < 10; i += 1) {
+        var dog = getRandomItem(dogImages);
+        photo[i].innerHTML = dog.image;
+    }
 }
 
 function generateOptions() {
